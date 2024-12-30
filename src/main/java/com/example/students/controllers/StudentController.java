ad003ba16a65e93a71e501aed0873074295ee8ea
neuriop <derter678ip@gmail.com>
2024-12-30 21:27:57 +0200

    fix commit

--- src/main/java/com/example/students/controllers/StudentController.java
+++ src/main/java/com/example/students/controllers/StudentController.java
@@ -24,6 +24,7 @@ public class StudentController {
         return ResponseEntity.ok(studentService.getStudentById(id));
     }
 
+    @PostMapping(value = "/add_student")
     public ResponseEntity<Boolean> addStudent(@RequestBody Student student){
         studentService.addStudent(student);
         return ResponseEntity.ok(true);
